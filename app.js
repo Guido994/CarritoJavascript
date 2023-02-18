a7845587c893d9afd2c35d95645cd9d617244650
Guido Lanteri <guidolanteri@hotmail.com>
2023-02-17 23:37:59 -0300

    CarritoDeCompras

---
+++ app.js
@@ -0,0 +1,118 @@
+const botónAgregarAlCarritoCompras = document.querySelectorAll('.agregarAlCarrito');
+botónAgregarAlCarritoCompras.forEach((botónAgregarAlCarrito) => {
+  botónAgregarAlCarrito.addEventListener('click', clickAgregarAlCarrito);
+});
+
+const botónComprar = document.querySelector('.botónComprar');
+botónComprar.addEventListener('click', clickBotónComprar);
+
+const contenedorItemsCarritoCompras = document.querySelector(
+  '.contenedorItemsCarritoCompras'
+);
+
+function clickAgregarAlCarrito(event) {
+  const botón = event.target;
+  const item = botón.closest('.item');
+
+  const itemTítulo = item.querySelector('.item-título').textContent;
+  const itemPrecio = item.querySelector('.item-precio').textContent;
+  const itemImagen = item.querySelector('.item-imagen').src;
+
+  agregarItemAlCarritoCompras(itemTítulo, itemPrecio, itemImagen);
+}
+
+function agregarItemAlCarritoCompras(itemTítulo, itemPrecio, itemImagen) {
+  const elementosTítulo = contenedorItemsCarritoCompras.getElementsByClassName(
+    'itemCarritoComprasTítulo'
+  );
+  for (let i = 0; i < elementosTítulo.length; i++) {
+    if (elementosTítulo[i].innerText === itemTítulo) {
+      let elementQuantity = elementosTítulo[
+        i
+      ].parentElement.parentElement.parentElement.querySelector(
+        '.cantidadItemsCarritoCompras'
+      );
+      elementQuantity.value++;
+      $('.toast').toast('show');
+      actualizarTotalCarritoCompras();
+      return;
+    }
+  }
+
+  const colaCarritoCompras = document.createElement('div');
+  const contenidoCarritoCompras = `
+  <div class="row itemCarritoCompras">
+        <div class="col-6">
+            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <img src=${itemImagen} class="imagen-carrito-compras">
+                <h6 class="shopping-cart-item-título itemCarritoComprasTítulo text-truncate ml-3 mb-0">${itemTítulo}</h6>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class="shopping-cart-precio d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+                <p class="item-precio mb-0 itemPrecioCarritoCompras">${itemPrecio}</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <div
+                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
+                <input class="input-carrito-compras cantidadItemsCarritoCompras" type="number"
+                    value="1">
+                <botón class="btn btn-danger botónQuitar" type="botón">X</botón>
+            </div>
+        </div>
+    </div>`;
+  colaCarritoCompras.innerHTML = contenidoCarritoCompras;
+  contenedorItemsCarritoCompras.append(colaCarritoCompras);
+
+  colaCarritoCompras
+    .querySelector('.botónQuitar')
+    .addEventListener('click', quitarItemCarritoCompras);
+
+  colaCarritoCompras
+    .querySelector('.cantidadItemsCarritoCompras')
+    .addEventListener('cambiar', cambiarCantidad);
+
+  actualizarTotalCarritoCompras();
+}
+
+function actualizarTotalCarritoCompras() {
+  let total = 0;
+  const totalCarritoCompras = document.querySelector('.totalCarritoCompras');
+
+  const itemsCarritoCompras = document.querySelectorAll('.itemCarritoCompras');
+
+  itemsCarritoCompras.forEach((itemCarritoCompras) => {
+    const elementoItemPrecioCarritoCompras = itemCarritoCompras.querySelector(
+      '.itemPrecioCarritoCompras'
+    );
+    const itemPrecioCarritoCompras = Number(
+      elementoItemPrecioCarritoCompras.textContent.replace('$', '')
+    );
+    const elementoCantidadItemsCarritoCompras = itemCarritoCompras.querySelector(
+      '.cantidadItemsCarritoCompras'
+    );
+    const cantidadItemsCarritoCompras = Number(
+      elementoCantidadItemsCarritoCompras.value
+    );
+    total = total + itemPrecioCarritoCompras * cantidadItemsCarritoCompras;
+  });
+  totalCarritoCompras.innerHTML = `$${total.toFixed(2)}`;
+}
+
+function quitarItemCarritoCompras(event) {
+  const botónClick = event.target;
+  botónClick.closest('.itemCarritoCompras').remove();
+  actualizarTotalCarritoCompras();
+}
+
+function cambiarCantidad(event) {
+  const input = event.target;
+  input.value <= 0 ? (input.value = 1) : null;
+  actualizarTotalCarritoCompras();
+}
+
+function clickBotónComprar() {
+  contenedorItemsCarritoCompras.innerHTML = '';
+  actualizarTotalCarritoCompras();
+}
